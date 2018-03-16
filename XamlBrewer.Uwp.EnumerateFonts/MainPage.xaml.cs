3b2882c5944a471de99bf73d6763be5a876f17f8
Diederik Krols <diederik_krols@hotmail.com>
2018-03-16 15:18:12 +0100

    First release.

--- XamlBrewer.Uwp.EnumerateFonts/MainPage.xaml.cs
+++ XamlBrewer.Uwp.EnumerateFonts/MainPage.xaml.cs
@@ -1,30 +1,50 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas.Text;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace XamlBrewer.Uwp.EnumerateFonts
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
+            FluentDesignify();
+
             this.InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            FontsCombo.SelectedIndex = 0;
+        }
+
+        public List<string> Fonts
+        {
+            get
+            {
+                return CanvasTextFormat.GetSystemFontFamilies().OrderBy(f => f).ToList();
+            }
+        }
+
+        private void FontsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextEditor.FontFamily = new FontFamily(FontsCombo.SelectedValue.ToString());
+        }
+
+        private void FluentDesignify()
+        {
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonForegroundColor = Colors.LightSlateGray;
         }
     }
 }
